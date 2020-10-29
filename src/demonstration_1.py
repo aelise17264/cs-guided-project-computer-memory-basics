7cedd79397c0d14ae421e7309003d04f53d331fa
unknown <aelise17264@gmail.com>
2020-10-29 12:14:27 -0600

    finished lecture

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -23,4 +23,37 @@ if there is a mathematical approach that you can take.*
 """
 def to_lower_case(string):
     # Your code here
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    valid = True
+    for char in string:
+        if char not in alpha + upper:
+            valid = False
+    if valid:
+        result = ""
+        for char in string:
+            if char in alpha:
+                result += char
+            else:
+                for i in range(len(upper)):
+                    if char == upper[i]:
+                        result += alpha[i]
+    return result
 
+    # encoded_chars = []
+    # for char in string:
+    #     encoded_chars.append(ord(char))
+
+    # for i in range(len(encoded_chars)):
+    #     if encoded_chars[i] > 64 and encoded_chars[i] < 91:
+    #         encoded_chars[i] += 32
+
+    # decoded_chars = []
+    # for n in encoded_chars:
+    #     decoded_chars.append(chr(n))
+
+    # return "".join(decoded_chars)
+
+print(to_lower_case("LambdaSchool"))
+print(to_lower_case("LLAMA"))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -16,6 +16,41 @@ numbers) but have the property that they don't have a + or - sign associated
 with them. Thus they are always non-negative (zero or positive). We use uint's
 when we know the value we are counting will always be non-negative."
 """
+
+#n is going to be a normal intiger & we are going to return an intiger
 def hamming_weight(n):
     # Your code here
+    #if we're given a "normal" unsigned intiger, how do we convert it to a bitwise representation?
+    #bitwise logical operators `&&`, `||` in js
+    #bitwise logical operators: `&`, `|`
 
+    #using & we have a way to check the rightmost bit of n to see if it equals 1
+    #the left & right shift operations
+    #<< or >>
+    #if & on the rightmost digit returns a 1, increment a counter
+    # >> by 1 bitwise digit
+    counter = 0
+    #when do we stop right shifting?
+    #we can stop right shifting when n ==0
+
+    #way 1
+    # while n != 0:
+    #     if n & 1 == 1:
+    #         counter += 1
+    #     #do the right shift
+    #     n = n>>1
+
+    #return the number of 1s in the bitwise representation of the number
+
+    #way 2
+    # bin_representation = bin(n)
+
+    # for i in range(len(bin_representation)):
+    #     if bin_representation[i] == '1':
+    #         counter += 1
+
+    # return counter
+
+    #way 3
+    return bin(n).count("1")
+print(hamming_weight(20))
